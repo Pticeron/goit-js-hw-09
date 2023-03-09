b724b548ca3c12715696d4f01f19f7c1c97aba7d
Inna Zorina <innazorina0101@gmail.com>
2023-03-09 20:59:31 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector(`button[data-start]`);
+const stopBtn = document.querySelector(`button[data-stop]`);
+const bodyEl = document.querySelector(`body`);
 
+let timerId = null;
+
+startBtn.disabled = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener("click", onStartClick);
+stopBtn.addEventListener("click", onStopClick);
+
+function onStartClick() {
+    timerId = setInterval(() => {
+        startBtn.disabled = true;
+        stopBtn.disabled = false; 
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+}
+
+function onStopClick() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerId);
+    console.log('Timer stopped');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
